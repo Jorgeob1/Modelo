fdd4af0dd318094b01fb090e3cd9873c4aa077db
Jorgeob1 <33682920+Jorgeob1@users.noreply.github.com>
2017-11-22 08:42:25 +0100

    primer ejercicio

---
+++ ejercicio_tres_tema_tres.py
@@ -0,0 +1,19 @@
+n1=int(input("primer numero"))
+n2=int(input("segundo numero"))
+n3=int(input("tercer numero"))
+if n1<n2:
+  x=n1
+else:
+ x=n2
+ if x<n3:
+   print(x)
+ else:
+  print(n3)
+
+
+   
+
+   
+   
+   
+  
