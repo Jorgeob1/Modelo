24df1bbb22c9a0dd77bfa79a979937a867968fb3
Jorgeob1 <33682920+Jorgeob1@users.noreply.github.com>
2018-01-17 09:08:32 +0100

    Add files via upload

---
+++ correccion ejercicio series 1.py
@@ -0,0 +1,13 @@
+A=int(input("primer numero entero"))
+B=int(input("segundo numero enteros"))
+if(A<B):
+  for c in range(A, B+1):
+    print(c)
+else:
+  for c in range(B, A+1):
+    print(c)
+
+
+
+
+
